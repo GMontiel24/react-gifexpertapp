e9c807e933e138bce3a45091e9403330f77c1b08
German Montiel <gmontiel@originsoftware.com.ar>
2020-11-10 00:23:52 -0300

    docs agregado

---
+++ docs/precache-manifest.7720fff4e526af02963d9dd80d22a346.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba481bd1c065b68fd2759267bfe12780",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c93f7492428ea55c1012",
+    "url": "/static/css/main.ec15fd37.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93f7492428ea55c1012",
+    "url": "/static/js/main.8a23bc08.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
